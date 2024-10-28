924e005d6a09d808b557d5f1370ae1d39d17fa71
Segni <segnitsega6@gmail.com>
2024-10-28 03:50:07 -0700

    project setup

---
+++ src/sections/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <a href="">
+                    <img src="/images/xora.svg" width={115} height={55} alt="logo"/>
+                </a>
+            </div>
+        </header>
+    )
+}
+export default Header
