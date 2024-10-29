b30a98d3f677959750bb69c2fb39156f2b2d0f32
Segni <segnitsega6@gmail.com>
2024-10-29 02:41:16 -0700

    added Features.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
