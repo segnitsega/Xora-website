e16b79825d37d8afd8a4a6a6b35b8ee5f0e7c187
Segni <segnitsega6@gmail.com>
2024-10-29 06:05:10 -0700

    created the Pricing.jsx compoonent

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
