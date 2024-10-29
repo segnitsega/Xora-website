be237d583d188993ec25a1545bdb72dfbbb6441c
Segni <segnitsega6@gmail.com>
2024-10-29 09:55:46 -0700

    Added a Testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 };
